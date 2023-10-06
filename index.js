f2cb1397b43f92bde3b3e419276e0d434667cf88
rae-2 <cloudandelion@gmail.com>
2023-10-06 13:12:02 -0400

    still single

---
+++ index.js
@@ -0,0 +1,53 @@
+const students = [
+    {
+        firstName: "hahah",
+        lastName: "hehehe",
+        graduated: false,
+        DOB: 2333,
+        siblings: ["eeeee"],
+        age: function () {
+            return year - this.DOB;
+        },
+    },
+    {
+        firstName: "hahhe",
+        lastName: "hehe",
+        graduated: false,
+        DOB: 23,
+        siblings: ["yahaha"],
+        age: function () {
+            return year - this.DOB;
+        },
+    },
+    {
+        firstName: "hah",
+        lastName: "hehahe",
+        graduated: true,
+        DOB: 3323,
+        siblings: ["wwwwddd"],
+        age: function () {
+            return year - this.DOB;
+        },
+    },
+    {
+        firstName: "hwwsse",
+        lastName: "heaaa",
+        graduated: true,
+        DOB: 7773,
+        siblings: ["ymemem"],
+        age: function () {
+            return year - this.DOB;
+        },
+    },
+];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//index starts at 0
+//console.log(numbers[numbers.length - 1]);
+//foreach is a for loop
+//arrow function ()=> auto returns output, this keyword points global
+//numbers.forEach((number, index) => console.log(number, index));
+//students.forEach((student) => console.log(student.firstName));
+
+const current = students.filter((student)=> student.graduated !== true);
+console.log(current);
